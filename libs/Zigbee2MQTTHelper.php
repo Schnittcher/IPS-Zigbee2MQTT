771ecf3c198adb4063d55532360b0c79737585e3
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-04-29 14:38:03 +0200

    fix setColorMOde

--- libs/Zigbee2MQTTHelper.php
+++ libs/Zigbee2MQTTHelper.php
@@ -43,7 +43,7 @@ trait Zigbee2MQTTHelper
     }
 
     public function setColorMode(int $mode) {
-        $Payload['color_mode'] = stravl($mode);
+        $Payload['color_mode'] = strval($mode);
         $PayloadJSON = json_encode($Payload, JSON_UNESCAPED_SLASHES);
         $this->publish($PayloadJSON);
     }
