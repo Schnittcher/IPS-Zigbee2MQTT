fc31aeec763eeff0846959be7de2795a2334ea17
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-06-18 11:18:42 +0200

    Version 2.0 Color im Webfornt hinzugefügt

--- IPS-Z2MDevice/module.php
+++ IPS-Z2MDevice/module.php
@@ -149,6 +149,18 @@ class IPS_Z2MDevice extends IPSModule
                     $this->RegisterVariableFloat('Z2M_Counter', $this->Translate('Counter'), '');
                     SetValue($this->GetIDForIdent('Z2M_Counter'), $Payload->counter);
                 }
+                if (property_exists($Payload, 'color')) {
+                    $this->SendDebug(__FUNCTION__. ' Color', $Payload->color->x,0);
+                    if (property_exists($Payload, 'brightness')) {
+                        $RGBColor = $this->CIEToRGB($Payload->color->x, $Payload->color->y, $Payload->brightness);
+                    } else {
+                        $RGBColor = $this->CIEToRGB($Payload->color->x,$Payload->color->y);
+                    }
+                    $this->SendDebug(__FUNCTION__. ' Color RGB HEX', $RGBColor,0);
+                    $this->RegisterVariableInteger('Z2M_Color', $this->Translate('Color'), 'HexColor');
+                    $this->EnableAction('Z2M_Color');
+                    SetValue($this->GetIDForIdent('Z2M_Color'), hexdec(($RGBColor)));
+                }
                 if (property_exists($Payload, 'sensitivity')) {
                     $this->RegisterVariableInteger('Z2M_Sensitivity', $this->Translate('Sensitivity'), 'Z2M.Sensitivity');
                     SetValue($this->GetIDForIdent('Z2M_Sensitivity'), $Payload->sensitivity);
--- libs/Zigbee2MQTTHelper.php
+++ libs/Zigbee2MQTTHelper.php
@@ -17,6 +17,10 @@ trait Zigbee2MQTTHelper
             case 'Z2M_Sensitivity':
                 $this->setSensitivity($Value);
                 break;
+            case 'Z2M_Color':
+                $this->SendDebug(__FUNCTION__.' Color', $Value,0);
+                $this->setColor($Value,'cie');
+                break;
             default:
                 $this->SendDebug('Request Action', 'No Action defined: ' . $Ident, 0);
                 break;
@@ -116,6 +120,74 @@ trait Zigbee2MQTTHelper
 
         return $cie;
     }
+
+    protected function CIEToRGB($x,$y, $brightness = 255)
+    {
+        $z = 1.0 - $x - $y;
+        $Y = $brightness / 255;
+        $X = ($Y / $y) * $x;
+        $Z = ($Y / $y) * $z;
+
+        $red = $X * 1.656492 - $Y * 0.354851 - $Z * 0.255038;
+        $green = -$X * 0.707196 + $Y * 1.655397 + $Z * 0.036152;
+        $blue = $X * 0.051713 - $Y * 0.121364 + $Z * 1.011530;
+
+        if ($red > $blue && $red > $green && $red > 1.0) {
+            $green = $green / $red;
+            $blue = $blue / $red;
+            $red = 1.0;
+        } elseif ($green > $blue && $green > $red && $green > 1.0) {
+
+            $red = $red / $green;
+            $blue = $blue / $green;
+            $green = 1.0;
+        } elseif ($blue > $red && $blue > $green && $blue > 1.0) {
+
+            $red = $red / $blue;
+            $green = $green / $blue;
+            $blue = 1.0;
+        }
+        $red = $red <= 0.0031308 ? 12.92 * $red : (1.0 + 0.055) * $red ** (1.0 / 2.4) - 0.055;
+        $green = $green <= 0.0031308 ? 12.92 * $green : (1.0 + 0.055) * $green ** (1.0 / 2.4) - 0.055;
+        $blue = $blue <= 0.0031308 ? 12.92 * $blue : (1.0 + 0.055) * $blue ** (1.0 / 2.4) - 0.055;
+
+        $red = ceil($red * 255);
+        $green = ceil($green * 255);
+        $blue = ceil($blue * 255);
+
+        $color = sprintf('#%02x%02x%02x', $red, $green, $blue);
+
+        return $color;
+    }
+      /**
+        $r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
+        $g = ($X * -1) * 0.509 + $Y * 1.412 + $Z * 0.066;
+        $b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;
+
+        $r = $r <= 0.0031308 ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
+        $g = $g <= 0.0031308 ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
+        $b = $b <= 0.0031308 ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;
+
+        $maxValue = max( $r , $g, $b );
+
+        $r = $r / $maxValue;
+        $g = $g / $maxValue;
+        $b = $b / $maxValue;
+
+        $r = $r * 255; if ($r < 0) $r = 255;
+        $g = $g * 255; if ($g < 0) $g = 255;
+        $b = $b * 255; if ($b < 0) $b = 255;
+
+        $r = dechex(round($r));
+        $g = dechex(round($g));
+        $b = dechex(round($b));
+
+        if (strlen($r) < 2)     $r = "0" + $r;
+        if (strlen($g) < 2)     $g = "0" + $g;
+        if (strlen($b) < 2)     $b = "0" + $b;
+
+        return "#".$r.$g.$b;
+       * **/
 
     protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
     {
