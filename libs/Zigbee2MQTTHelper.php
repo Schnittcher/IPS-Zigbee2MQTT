b32697a32e51b5ea7fa0e9db0c1d08ef79884750
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-06-18 09:19:31 +0000

    Apply fixes from StyleCI

--- libs/Zigbee2MQTTHelper.php
+++ libs/Zigbee2MQTTHelper.php
@@ -18,8 +18,8 @@ trait Zigbee2MQTTHelper
                 $this->setSensitivity($Value);
                 break;
             case 'Z2M_Color':
-                $this->SendDebug(__FUNCTION__.' Color', $Value,0);
-                $this->setColor($Value,'cie');
+                $this->SendDebug(__FUNCTION__ . ' Color', $Value, 0);
+                $this->setColor($Value, 'cie');
                 break;
             default:
                 $this->SendDebug('Request Action', 'No Action defined: ' . $Ident, 0);
@@ -121,7 +121,7 @@ trait Zigbee2MQTTHelper
         return $cie;
     }
 
-    protected function CIEToRGB($x,$y, $brightness = 255)
+    protected function CIEToRGB($x, $y, $brightness = 255)
     {
         $z = 1.0 - $x - $y;
         $Y = $brightness / 255;
@@ -137,12 +137,10 @@ trait Zigbee2MQTTHelper
             $blue = $blue / $red;
             $red = 1.0;
         } elseif ($green > $blue && $green > $red && $green > 1.0) {
-
             $red = $red / $green;
             $blue = $blue / $green;
             $green = 1.0;
         } elseif ($blue > $red && $blue > $green && $blue > 1.0) {
-
             $red = $red / $blue;
             $green = $green / $blue;
             $blue = 1.0;
@@ -159,36 +157,35 @@ trait Zigbee2MQTTHelper
 
         return $color;
     }
-      /**
-        $r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
-        $g = ($X * -1) * 0.509 + $Y * 1.412 + $Z * 0.066;
-        $b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;
 
-        $r = $r <= 0.0031308 ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
-        $g = $g <= 0.0031308 ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
-        $b = $b <= 0.0031308 ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;
-
-        $maxValue = max( $r , $g, $b );
-
-        $r = $r / $maxValue;
-        $g = $g / $maxValue;
-        $b = $b / $maxValue;
-
-        $r = $r * 255; if ($r < 0) $r = 255;
-        $g = $g * 255; if ($g < 0) $g = 255;
-        $b = $b * 255; if ($b < 0) $b = 255;
-
-        $r = dechex(round($r));
-        $g = dechex(round($g));
-        $b = dechex(round($b));
-
-        if (strlen($r) < 2)     $r = "0" + $r;
-        if (strlen($g) < 2)     $g = "0" + $g;
-        if (strlen($b) < 2)     $b = "0" + $b;
-
-        return "#".$r.$g.$b;
-       * **/
-
+    /**
+     * $r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
+     * $g = ($X * -1) * 0.509 + $Y * 1.412 + $Z * 0.066;
+     * $b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;
+     *
+     * $r = $r <= 0.0031308 ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
+     * $g = $g <= 0.0031308 ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
+     * $b = $b <= 0.0031308 ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;.
+     * $maxValue = max( $r , $g, $b );
+     *
+     * $r = $r / $maxValue;
+     * $g = $g / $maxValue;
+     * $b = $b / $maxValue;
+     *
+     * $r = $r * 255; if ($r < 0) $r = 255;
+     * $g = $g * 255; if ($g < 0) $g = 255;
+     * $b = $b * 255; if ($b < 0) $b = 255;
+     *
+     * $r = dechex(round($r));
+     * $g = dechex(round($g));
+     * $b = dechex(round($b));
+     *
+     * if (strlen($r) < 2)     $r = "0" + $r;
+     * if (strlen($g) < 2)     $g = "0" + $g;
+     * if (strlen($b) < 2)     $b = "0" + $b;
+     *
+     * return "#".$r.$g.$b;
+     * **/
     protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
     {
         if (!IPS_VariableProfileExists($Name)) {
