05e09a11089231e07881030322a2f5d18ceb9fd4
Kai <kai.schnittcher@gmail.com>
2019-12-11 08:12:05 +0100

    Fix RequestAction für Z2M_Statel1 und Z2M_Statel2

--- libs/Zigbee2MQTTHelper.php
+++ libs/Zigbee2MQTTHelper.php
@@ -22,10 +22,10 @@ trait Zigbee2MQTTHelper
                 $this->SwitchMode($Value);
                 break;
             case 'Z2M_Statel1':
-                $this->Command('l1/set', $Value);
+                $this->Command('l1/set', $this->OnOff($Value));
                 break;
             case 'Z2M_Statel2':
-                $this->Command('l2/set', $Value);
+                $this->Command('l2/set', $this->OnOff($Value));
                 break;
             case 'Z2M_Sensitivity':
                 $this->setSensitivity($Value);
@@ -72,14 +72,7 @@ trait Zigbee2MQTTHelper
 
     public function SwitchMode(bool $value)
     {
-        switch ($value) {
-            case true:
-                $state = 'ON';
-                break;
-            case false:
-                $state = 'OFF';
-                break;
-        }
+        $this->OnOff($Value);
         $Payload['state'] = $state;
         $PayloadJSON = json_encode($Payload, JSON_UNESCAPED_SLASHES);
         $this->publish($PayloadJSON);
@@ -180,34 +173,6 @@ trait Zigbee2MQTTHelper
         return $color;
     }
 
-    /**
-     * $r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
-     * $g = ($X * -1) * 0.509 + $Y * 1.412 + $Z * 0.066;
-     * $b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;.
-     *
-     * $r = $r <= 0.0031308 ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
-     * $g = $g <= 0.0031308 ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
-     * $b = $b <= 0.0031308 ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;.
-     * $maxValue = max( $r , $g, $b );
-     *
-     * $r = $r / $maxValue;
-     * $g = $g / $maxValue;
-     * $b = $b / $maxValue;
-     *
-     * $r = $r * 255; if ($r < 0) $r = 255;
-     * $g = $g * 255; if ($g < 0) $g = 255;
-     * $b = $b * 255; if ($b < 0) $b = 255;
-     *
-     * $r = dechex(round($r));
-     * $g = dechex(round($g));
-     * $b = dechex(round($b));
-     *
-     * if (strlen($r) < 2)     $r = "0" + $r;
-     * if (strlen($g) < 2)     $g = "0" + $g;
-     * if (strlen($b) < 2)     $b = "0" + $b;
-     *
-     * return "#".$r.$g.$b;
-     * */
     protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
     {
         if (!IPS_VariableProfileExists($Name)) {
@@ -269,6 +234,19 @@ trait Zigbee2MQTTHelper
         foreach ($Associations as $Association) {
             IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
         }
+    }
+
+    private function OnOff(string $Value)
+    {
+        switch ($value) {
+            case true:
+                $state = 'ON';
+                break;
+            case false:
+                $state = 'OFF';
+                break;
+        }
+        return $state;
     }
 
     private function publish($payload)
