58a2eaf373d58b5cf8ff0fb3f4c5550af2aabcda
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-05-26 12:06:57 +0200

    Topic im Konfigurator angepasst

--- Configurator/module.php
+++ Configurator/module.php
@@ -12,7 +12,7 @@ class Zigbee2MQTTConfigurator extends IPSModule
         //Never delete this line!
         parent::Create();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
-        $this->RegisterPropertyString('MQTTTopic', '');
+        $this->RegisterPropertyString('MQTTTopic', 'bridge');
         $this->SetBuffer('Devices', '{}');
         $this->SetBuffer('Groups', '{}');
 
