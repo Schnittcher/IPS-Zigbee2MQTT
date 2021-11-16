e522eebc895ec819c9070beac4290e7fc2fff33a
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-11-16 15:32:34 +0100

    Exposes auf Array prüfen und nicht auf Null

--- libs/Zigbee2MQTTHelper.php
+++ libs/Zigbee2MQTTHelper.php
@@ -158,7 +158,7 @@ trait Zigbee2MQTTHelper
 
             $Payload = json_decode($Buffer->Payload, true);
             if (fnmatch('symcon/' . $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/deviceInfo', $Buffer->Topic)) {
-                if (!is_null($Payload['_definition']['exposes'])) {
+                if (is_array($Payload['_definition']['exposes'])) {
                     $this->mapExposesToVariables($Payload['_definition']['exposes']);
                 }
             }
