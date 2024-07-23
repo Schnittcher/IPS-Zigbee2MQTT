2434a9aca7ad68a2822f2c04f81a2a03e660cee8
Michael Tröger <mt@neo-ami.de>
2024-07-23 17:54:13 +0200

    Refactoring the (not working) bridge module

--- Bridge/module.php
+++ Bridge/module.php
@@ -1,13 +1,15 @@
 <?php
 
 declare(strict_types=1);
-require_once __DIR__ . '/../libs/MQTTHelper.php';
-require_once __DIR__ . '/../libs/Zigbee2MQTTBridgeHelper.php';
-require_once __DIR__ . '/../libs/Zigbee2MQTTHelper.php';
+require_once dirname(__DIR__) . '/libs/MQTTHelper.php';
+require_once dirname(__DIR__) . '/libs/VariableProfileHelper.php';
+require_once dirname(__DIR__) . '/libs/Zigbee2MQTTBridgeHelper.php';
+require_once dirname(__DIR__) . '/libs/Zigbee2MQTTHelper.php';
 
 class Zigbee2MQTTBridge extends IPSModule
 {
     use \Zigbee2MQTT\Zigbee2MQTTBridgeHelper;
+    use \Zigbee2MQTT\VariableProfileHelper;
     use \Zigbee2MQTT\MQTTHelper;
 
     public function Create()
@@ -15,7 +17,15 @@ class Zigbee2MQTTBridge extends IPSModule
         //Never delete this line!
         parent::Create();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
-        $this->RegisterPropertyString('MQTTTopic', 'bridge');
+        $this->RegisterPropertyString('MQTTBaseTopic', 'zigbee2mqtt');
+        $Version = 'unknown';
+        $File = file(dirname(__DIR__) . '/libs/IPSymconExtension.js');
+        $Start = strpos($File[2], 'Version: ');
+        if ($Start) {
+            $Version = trim(substr($File[2], $Start + strlen('Version: ')));
+        }
+        $this->SetBuffer('actualExtensionVersion', $Version);
+        $this->SetBuffer('ExtensionFilename', '');
     }
 
     public function ApplyChanges()
@@ -23,109 +33,132 @@ class Zigbee2MQTTBridge extends IPSModule
         //Never delete this line!
         parent::ApplyChanges();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
-        //Setze Filter für ReceiveData
-        $MQTTTopic = $this->ReadPropertyString('MQTTTopic');
-        $this->SetReceiveDataFilter('.*' . $MQTTTopic . '.*');
+        $BaseTopic = $this->ReadPropertyString('MQTTBaseTopic');
+        if (empty($BaseTopic)) {
+            $this->SetStatus(IS_INACTIVE);
+            $this->SetReceiveDataFilter('NOTHING_TO_RECEIVE'); //block all
+        } else {
+            $this->SetStatus(IS_ACTIVE);
+            //Setze Filter für ReceiveData
+            $this->SetReceiveDataFilter('.*"Topic":"' . $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/.*');
+        }
+        $this->RegisterProfileIntegerEx('Z2M.bridge.restart', '', '', '', [
+            [0, $this->Translate('Restart'), '', 0xFF0000],
+        ]);
+        $this->RegisterProfileStringEx('Z2M.brigde.loglevel', '', '', '', [
+            ['error', $this->Translate('Error'), '', 0x00FF00],
+            ['warning', $this->Translate('Warning'), '', 0x00FF00],
+            ['info', $this->Translate('Info'), '', 0x00FF00],
+            ['debug', $this->Translate('Debug'), '', 0x00FF00],
+
+        ]);
     }
 
     public function ReceiveData($JSONString)
     {
-        $this->SendDebug('JSON', $JSONString, 0);
-        if (!empty($this->ReadPropertyString('MQTTTopic'))) {
-            $Buffer = json_decode($JSONString);
-            // Buffer decodieren und in eine Variable schreiben
-            $this->SendDebug('MQTT Topic', $Buffer->Topic, 0);
-            $this->SendDebug('MQTT Payload', $Buffer->Payload, 0);
-            if (property_exists($Buffer, 'Topic')) {
-                $Payload = json_decode($Buffer->Payload);
-                if (fnmatch('*state*', $Buffer->Topic)) {
-                    $this->RegisterVariableBoolean('Z2M_State', $this->Translate('State'), '');
-                    switch ($Buffer->Payload) {
-                            case 'online':
-                                SetValue($this->GetIDForIdent('Z2M_State'), true);
-                                break;
-                            case 'offline':
-                                SetValue($this->GetIDForIdent('Z2M_State'), false);
-                                break;
-                            default:
-                                $this->SendDebug('Bridge State', 'Invalid Payload' . $Buffer->Payload, 0);
-                        }
-                }
-                if (fnmatch('*config*', $Buffer->Topic)) {
-                    if (property_exists($Payload, 'log_level')) {
-                        $this->RegisterVariableString('Z2M_Log_Level', $this->Translate('Log Level'), '');
-                        SetValue($this->GetIDForIdent('Z2M_Log_Level'), $Payload->log_level);
-                    }
-                    if (property_exists($Payload, 'permit_join')) {
-                        $this->RegisterVariableString('Z2M_Permit_Join', $this->Translate('Permit Join'), '');
-                        SetValue($this->GetIDForIdent('Z2M_Permit_Join'), $Payload->permit_join);
-                    }
-                }
-                if (fnmatch('*event*', $Buffer->Topic)) {
-                    if (property_exists($Payload, 'type')) {
-                        switch ($Payload->type) {
-                             case 'device_announce':
-                                 $this->RegisterVariableString('Z2M_Device_Announce', $this->Translate('Announce'), '');
-                                 SetValue($this->GetIDForIdent('Z2M_Device_Announce'), $Buffer->Payload);
-                                 break;
-                         }
-                    }
-                }
-                if (fnmatch('*log*', $Buffer->Topic)) {
-                    if (property_exists($Payload, 'type')) {
-                        switch ($Payload->type) {
-                            case 'pairing':
-                                $this->RegisterVariableString('Z2M_Pairing', $this->Translate('Pairing'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Pairing'), $Buffer->Payload);
-                                break;
-                            case 'device_connected':
-                                $this->RegisterVariableString('Z2M_Device_Connected', $this->Translate('Device Connected'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Connected'), $Buffer->Payload);
-                                break;
-                            case 'device removed':
-                                $this->RegisterVariableString('Z2M_Device_Removed', $this->Translate('Device Removed'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Removed'), $Buffer->Payload);
-                                break;
-                            case 'device_banned':
-                                $this->RegisterVariableString('Z2M_Device_Banned', $this->Translate('Device Banned'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Banned'), $Buffer->Payload);
-                                break;
-                            case 'device_renamed':
-                                $this->RegisterVariableString('Z2M_Device_Renamed', $this->Translate('Device Renamed'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Renamed'), $Payload->message->from . ' >> ' . $Payload->message->to);
-                                break;
-                            case 'device_bind':
-                                $this->RegisterVariableString('Z2M_Device_bind', $this->Translate('Device Bind'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_bind'), $Buffer->Payload);
-                                break;
-                            case 'device_unbind':
-                                $this->RegisterVariableString('Z2M_Device_Unbind', $this->Translate('Device Unbind'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Unbind'), $Buffer->Payload);
-                                break;
-                            case 'device_group_add':
-                                $this->RegisterVariableString('Z2M_Device_Group_Add', $this->Translate('Device Group Add'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Group_Add'), $Buffer->Payload);
-                                break;
-                            case 'device_group_remove':
-                                $this->RegisterVariableString('Z2M_Device_Group_Remove', $this->Translate('Device Group Remove'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Group_Remove'), $Buffer->Payload);
-                                break;
-                            case 'device_group_remove_all':
-                                $this->RegisterVariableString('Z2M_Device_Group_Remove_All', $this->Translate('Device Group Remove All'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Group_Remove_All'), $Buffer->Payload);
-                                break;
-                            case 'devices':
-                                $this->RegisterVariableString('Z2M_Devices', $this->Translate('Devices'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Devices'), $Buffer->Payload);
-                                break;
-                            case 'device_publish_error':
-                                $this->RegisterVariableString('Z2M_Device_Publish_Error', $this->Translate('Device Publish Error'), '');
-                                SetValue($this->GetIDForIdent('Z2M_Device_Publish_Error'), $Payload->device_publish_error);
-                                break;
-                        }
-                    }
-                }
-            }
+        $BaseTopic = $this->ReadPropertyString('MQTTBaseTopic');
+        if (empty($BaseTopic)) {
+            return '';
         }
+        $this->SendDebug('JSON', $JSONString, 0);
+
+        $Buffer = json_decode($JSONString, true);
+        if (!isset($Buffer['Topic'])) {
+            return '';
+        }
+        $ReceiveTopic = $Buffer['Topic'];
+        $this->SendDebug('MQTT FullTopic', $ReceiveTopic, 0);
+        $Topic = substr($ReceiveTopic, strlen($BaseTopic . '/bridge/'));
+        $this->SendDebug('MQTT Topic', $Topic, 0);
+        $this->SendDebug('MQTT Payload', $Buffer['Payload'], 0);
+        $Payload = json_decode($Buffer['Payload'], true);
+        switch ($Topic) {
+            case 'state':
+                $this->RegisterVariableBoolean('state', $this->Translate('State'));
+                $this->SetValue('state', $Payload['state'] == 'online');
+                break;
+            case 'info':
+                if (isset($Payload['log_level'])) {
+                    $this->RegisterVariableString('log_level', $this->Translate('Log Level'), 'Z2M.brigde.loglevel');
+                    $this->EnableAction('log_level');
+                    $this->SetValue('log_level', $Payload['log_level']);
+                }
+                if (isset($Payload['permit_join'])) {
+                    $this->RegisterVariableBoolean('permit_join', $this->Translate('Permit Join'), '~Switch');
+                    $this->EnableAction('permit_join');
+                    $this->SetValue('permit_join', $Payload['permit_join']);
+                }
+                if (isset($Payload['restart_required'])) {
+                    $this->RegisterVariableBoolean('restart_required', $this->Translate('Restart Required'));
+                    $this->SetValue('restart_required', $Payload['restart_required']);
+                    $this->RegisterVariableInteger('restart_request', $this->Translate('Restart Request'), 'Z2M.bridge.restart');
+                    $this->EnableAction('restart_request');
+                }
+                if (isset($Payload['version'])) {
+                    $this->RegisterVariableString('version', $this->Translate('Version'));
+                    $this->SetValue('version', $Payload['version']);
+                }
+                if (isset($Payload['network'])) {
+                    $this->RegisterVariableInteger('network_channel', $this->Translate('Network Channel'));
+                    $this->SetValue('network_channel', $Payload['network']['channel']);
+                }
+                break;
+            case 'extensions':
+                $foundExtension = false;
+                foreach ($Payload as $Extension) {
+                    if (strpos($Extension['code'], 'class IPSymconExtension')) {
+                        $foundExtension = true;
+                        $this->SetBuffer('ExtensionName', $Extension['name']);
+                        $Version = 'unknown';
+                        $Lines = explode("\n", $Extension['code']);
+                        $Start = strpos($Lines[2], 'Version: ');
+                        if ($Start) {
+                            $Version = trim(substr($Lines[2], $Start + strlen('Version: ')));
+                        }
+                        $this->RegisterVariableString('extension_version', $this->Translate('Extension Version'));
+                        $this->SetValue('extension_version', $Version);
+                        $this->RegisterVariableBoolean('extension_actual', $this->Translate('Extension Actual'));
+                        $this->SetValue('extension_actual', $this->GetBuffer('actualExtensionVersion') == $Version);
+                        $this->SendDebug('actualExtensionVersion', $this->GetBuffer('actualExtensionVersion'), 0);
+                        break;
+                    }
+                }
+                $this->RegisterVariableBoolean('extension_loaded', $this->Translate('Extension Loaded'));
+                $this->SetValue('extension_loaded', $foundExtension);
+                break;
+        }
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+            case 'permit_join':
+                $this->SetPermitJoin((bool) $Value);
+                break;
+            case 'log_level':
+                $this->SetLogLevel((string) $Value);
+                break;
+            case'restart_request':
+                $this->Restart();
+                break;
+        }
+    }
+
+    public function InstallSymconExtension()
+    {
+        $ExtensionName = $this->GetBuffer('ExtensionName');
+        if (empty($ExtensionName)) {
+            $ExtensionName = 'IPSymconExtension.js';
+        }
+        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Data['PacketType'] = 3;
+        $Data['QualityOfService'] = 0;
+        $Data['Retain'] = false;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/request/extension/save';
+        $Data['Payload'] = json_encode(['name'=>$ExtensionName, 'code'=>file_get_contents(dirname(__DIR__) . '/libs/IPSymconExtension.js')]);
+        $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
+        $this->SendDebug(__FUNCTION__ . 'Restart', $Data['Topic'], 0);
+        $this->SendDebug(__FUNCTION__, $DataJSON, 0);
+        $this->SendDataToParent($DataJSON);
     }
 }
--- libs/Zigbee2MQTTBridgeHelper.php
+++ libs/Zigbee2MQTTBridgeHelper.php
@@ -6,13 +6,59 @@ namespace Zigbee2MQTT;
 
 trait Zigbee2MQTTBridgeHelper
 {
+    /** todo
+     * add transaction in Request ( root name + millis)
+     * check response in receivedata
+     */
+    public function SetPermitJoin(bool $PermitJoin)
+    {
+        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Data['PacketType'] = 3;
+        $Data['QualityOfService'] = 0;
+        $Data['Retain'] = false;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/request/permit_join';
+        $Data['Payload'] = json_encode(['value'=>$PermitJoin]);
+        $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
+        $this->SendDebug(__FUNCTION__ . 'SetPermitJoin', $Data['Topic'], 0);
+        $this->SendDebug(__FUNCTION__, $DataJSON, 0);
+        $this->SendDataToParent($DataJSON);
+    }
+
+    public function SetLogLevel(string $LogLevel)
+    {
+        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Data['PacketType'] = 3;
+        $Data['QualityOfService'] = 0;
+        $Data['Retain'] = false;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/request/options';
+        $Data['Payload'] = json_encode(['options' =>['advanced' => ['log_level'=> $LogLevel]]]);
+        $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
+        $this->SendDebug(__FUNCTION__ . 'SetLogLevel', $Data['Topic'], 0);
+        $this->SendDebug(__FUNCTION__, $DataJSON, 0);
+        $this->SendDataToParent($DataJSON);
+    }
+
+    public function Restart()
+    {
+        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
+        $Data['PacketType'] = 3;
+        $Data['QualityOfService'] = 0;
+        $Data['Retain'] = false;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/request/restart';
+        $Data['Payload'] = '';
+        $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
+        $this->SendDebug(__FUNCTION__ . 'Restart', $Data['Topic'], 0);
+        $this->SendDebug(__FUNCTION__, $DataJSON, 0);
+        $this->SendDataToParent($DataJSON);
+    }
+
     public function AddGroup(string $group_name, string $friendly_name)
     {
         $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/add';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/add';
         $Data['Payload'] = $friendly_name;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Add Group Topic', $Data['Topic'], 0);
@@ -26,7 +72,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/remove';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/remove';
         $Data['Payload'] = $friendly_name;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Remove Group Topic', $Data['Topic'], 0);
@@ -40,7 +86,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/remove_all';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/group/' . $group_name . '/remove_all';
         $Data['Payload'] = $friendly_name;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Publish Topic', $Data['Topic'], 0);
@@ -54,7 +100,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/bind/' . $source_device;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/bind/' . $source_device;
         $Data['Payload'] = $target_device;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Bind Topic', $Data['Topic'], 0);
@@ -68,7 +114,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/unbind/' . $source_device;
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/unbind/' . $source_device;
         $Data['Payload'] = $target_device;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Unbind Topic', $Data['Topic'], 0);
@@ -82,7 +128,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/device/' . $friendly_name . '/get_group_membership';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/device/' . $friendly_name . '/get_group_membership';
         $Data['Payload'] = '';
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'getGroupMembership Topic', $Data['Topic'], 0);
@@ -96,7 +142,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/networkmap';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/networkmap';
         $Data['Payload'] = 'graphviz';
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Bind Topic', $Data['Topic'], 0);
@@ -110,7 +156,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/rename';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/rename';
 
         $Payload['old'] = $old_friendly_name;
         $Payload['new'] = $new_friendly_name;
@@ -128,7 +174,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/ban';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/ban';
         $Data['Payload'] = $friendly_name;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Bind Topic', $Data['Topic'], 0);
@@ -142,7 +188,7 @@ trait Zigbee2MQTTBridgeHelper
         $Data['PacketType'] = 3;
         $Data['QualityOfService'] = 0;
         $Data['Retain'] = false;
-        $Data['Topic'] = MQTT_GROUP_TOPIC . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/remove';
+        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/' . $this->ReadPropertyString('MQTTTopic') . '/config/remove';
         $Data['Payload'] = $friendly_name;
         $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
         $this->SendDebug(__FUNCTION__ . 'Bind Topic', $Data['Topic'], 0);
