f7dd37c1d9f2b67dd6c9a407640db30b3ce34f61
Michael Tröger <mt@neo-ami.de>
2024-07-28 17:12:28 +0200

    no message

--- Bridge/module.php
+++ Bridge/module.php
@@ -1,16 +1,30 @@
 <?php
 
 declare(strict_types=1);
-require_once dirname(__DIR__) . '/libs/MQTTHelper.php';
+require_once dirname(__DIR__) . '/libs/BufferHelper.php';
+require_once dirname(__DIR__) . '/libs/SemaphoreHelper.php';
 require_once dirname(__DIR__) . '/libs/VariableProfileHelper.php';
-require_once dirname(__DIR__) . '/libs/Zigbee2MQTTBridgeHelper.php';
-require_once dirname(__DIR__) . '/libs/Zigbee2MQTTHelper.php';
 
+/**
+ * @property array $TransactionData
+ * @property string $actualExtensionVersion
+ * @property string $ExtensionFilename
+ * @property string $ConfigLastSeen
+ */
 class Zigbee2MQTTBridge extends IPSModule
 {
-    use \Zigbee2MQTT\Zigbee2MQTTBridgeHelper;
+    use \Zigbee2MQTT\BufferHelper;
+    use \Zigbee2MQTT\Semaphore;
     use \Zigbee2MQTT\VariableProfileHelper;
-    use \Zigbee2MQTT\MQTTHelper;
+
+    private static $MQTTDataArray = [
+        'DataID'           => '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}',
+        'PacketType'       => 3,
+        'QualityOfService' => 0,
+        'Retain'           => false,
+        'Topic'            => '',
+        'Payload'          => ''
+    ];
 
     public function Create()
     {
@@ -24,12 +38,15 @@ class Zigbee2MQTTBridge extends IPSModule
         if ($Start) {
             $Version = trim(substr($File[2], $Start + strlen('Version: ')));
         }
-        $this->SetBuffer('actualExtensionVersion', $Version);
-        $this->SetBuffer('ExtensionFilename', '');
+        $this->actualExtensionVersion = $Version;
+        $this->ExtensionFilename = '';
+        $this->ConfigLastSeen = 'epoch';
+        $this->TransactionData = [];
     }
 
     public function ApplyChanges()
     {
+        $this->TransactionData = [];
         //Never delete this line!
         parent::ApplyChanges();
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
@@ -50,7 +67,6 @@ class Zigbee2MQTTBridge extends IPSModule
             ['warning', $this->Translate('Warning'), '', 0x00FF00],
             ['info', $this->Translate('Information'), '', 0x00FF00],
             ['debug', $this->Translate('Debug'), '', 0x00FF00],
-
         ]);
     }
 
@@ -60,7 +76,7 @@ class Zigbee2MQTTBridge extends IPSModule
         if (empty($BaseTopic)) {
             return '';
         }
-        $this->SendDebug('JSON', $JSONString, 0);
+        $this->SendDebug('ReceiveData', $JSONString, 0);
 
         $Buffer = json_decode($JSONString, true);
         if (!isset($Buffer['Topic'])) {
@@ -69,10 +85,28 @@ class Zigbee2MQTTBridge extends IPSModule
         $ReceiveTopic = $Buffer['Topic'];
         $this->SendDebug('MQTT FullTopic', $ReceiveTopic, 0);
         $Topic = substr($ReceiveTopic, strlen($BaseTopic . '/bridge/'));
+        $Topics = explode('/', $Topic);
+        $Topic = array_shift($Topics);
         $this->SendDebug('MQTT Topic', $Topic, 0);
         $this->SendDebug('MQTT Payload', $Buffer['Payload'], 0);
         $Payload = json_decode($Buffer['Payload'], true);
         switch ($Topic) {
+            case 'request': //nothing todo
+                break;
+            case 'response': //response from request
+                if (isset($Payload['transaction'])) {
+                    $this->UpdateTransaction($Payload);
+                    break;
+                }
+                if (count($Topics)) {
+                    if ($Topics[0] == 'networkmap') {
+                        if ($Payload['status'] == 'ok') {
+                            $this->RegisterVariableString($Payload['data']['type'], $this->Translate('Network Map'));
+                            $this->SetValue($Payload['data']['type'], $Payload['data']['value']);
+                        }
+                    }
+                }
+                break;
             case 'state':
                 $this->RegisterVariableBoolean('state', $this->Translate('State'));
                 $this->SetValue('state', $Payload['state'] == 'online');
@@ -98,6 +132,13 @@ class Zigbee2MQTTBridge extends IPSModule
                     $this->RegisterVariableString('version', $this->Translate('Version'));
                     $this->SetValue('version', $Payload['version']);
                 }
+                if (isset($Payload['config']['advanced']['last_seen'])) {
+                    $this->SendDebug('last_seen', $Payload['config']['advanced']['last_seen'], 0);
+                    $this->ConfigLastSeen = $Payload['config']['advanced']['last_seen'];
+                    if ($Payload['config']['advanced']['last_seen'] != 'epoch') {
+                        $this->LogMessage($this->Translate('Wrong last_seen setting in Zigbee2MQTT. Please set last_seen to epoch.'), KL_ERROR);
+                    }
+                }
                 if (isset($Payload['network'])) {
                     $this->RegisterVariableInteger('network_channel', $this->Translate('Network Channel'));
                     $this->SetValue('network_channel', $Payload['network']['channel']);
@@ -108,7 +149,7 @@ class Zigbee2MQTTBridge extends IPSModule
                 foreach ($Payload as $Extension) {
                     if (strpos($Extension['code'], 'class IPSymconExtension')) {
                         $foundExtension = true;
-                        $this->SetBuffer('ExtensionName', $Extension['name']);
+                        $this->ExtensionName = $Extension['name'];
                         $Version = 'unknown';
                         $Lines = explode("\n", $Extension['code']);
                         $Start = strpos($Lines[2], 'Version: ');
@@ -118,14 +159,21 @@ class Zigbee2MQTTBridge extends IPSModule
                         $this->RegisterVariableString('extension_version', $this->Translate('Extension Version'));
                         $this->SetValue('extension_version', $Version);
                         $this->RegisterVariableBoolean('extension_is_current', $this->Translate('Extension is up to date'));
-                        $this->SetValue('extension_is_current', $this->GetBuffer('actualExtensionVersion') == $Version);
+                        $this->SetValue('extension_is_current', $this->actualExtensionVersion == $Version);
+                        if ($this->actualExtensionVersion != $Version) {
+                            $this->LogMessage($this->Translate('Symcon Extension in Zigbee2MQTT is outdated. Please update the extension.'), KL_ERROR);
+                        }
                         break;
                     }
                 }
                 $this->RegisterVariableBoolean('extension_loaded', $this->Translate('Extension Loaded'));
                 $this->SetValue('extension_loaded', $foundExtension);
+                if (!$foundExtension) {
+                    $this->LogMessage($this->Translate('No Symcon Extension in Zigbee2MQTT installed. Please install the extension.'), KL_ERROR);
+                }
                 break;
         }
+        return '';
     }
 
     public function RequestAction($Ident, $Value)
@@ -143,21 +191,312 @@ class Zigbee2MQTTBridge extends IPSModule
         }
     }
 
+    public function GetConfigurationForm()
+    {
+        $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+        if ($this->GetValue('extension_loaded') && $this->GetValue('extension_is_current')) {
+            $Form['actions'][0]['enabled'] = false;
+            $Form['actions'][0]['label'] = $this->Translate('Symcon-Extension is up-to-date');
+        }
+        if ($this->ConfigLastSeen == 'epoch') {
+            $Form['actions'][1]['enabled'] = false;
+            $Form['actions'][1]['label'] = $this->Translate('last_seen setting is correct');
+        }
+        return json_encode($Form);
+    }
+
     public function InstallSymconExtension()
     {
-        $ExtensionName = $this->GetBuffer('ExtensionName');
-        if (empty($ExtensionName)) {
+        if (empty($this->ExtensionName)) {
             $ExtensionName = 'IPSymconExtension.js';
         }
-        $Data['DataID'] = '{043EA491-0325-4ADD-8FC2-A30C8EEB4D3F}';
-        $Data['PacketType'] = 3;
-        $Data['QualityOfService'] = 0;
-        $Data['Retain'] = false;
-        $Data['Topic'] = $this->ReadPropertyString('MQTTBaseTopic') . '/bridge/request/extension/save';
-        $Data['Payload'] = json_encode(['name'=>$ExtensionName, 'code'=>file_get_contents(dirname(__DIR__) . '/libs/IPSymconExtension.js')]);
-        $DataJSON = json_encode($Data, JSON_UNESCAPED_SLASHES);
-        $this->SendDebug(__FUNCTION__ . 'Restart', $Data['Topic'], 0);
-        $this->SendDebug(__FUNCTION__, $DataJSON, 0);
+        $Topic = '/bridge/request/extension/save';
+        $Payload = ['name'=>$ExtensionName, 'code'=>file_get_contents(dirname(__DIR__) . '/libs/IPSymconExtension.js')];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function SetLastSeen()
+    {
+        $Topic = '/bridge/request/options';
+        $Payload = [
+            'options'=> [
+                'advanced'=> [
+                    'last_seen'=> 'epoch'
+                ]
+            ]
+        ];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function SetPermitJoin(bool $PermitJoin)
+    {
+        $Topic = '/bridge/request/permit_join';
+        $Payload = ['value'=>$PermitJoin];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function SetLogLevel(string $LogLevel)
+    {
+        $Topic = '/bridge/request/options';
+        $Payload = ['options' =>['advanced' => ['log_level'=> $LogLevel]]];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function Restart()
+    {
+        $Topic = '/bridge/request/restart';
+        $Result = $this->SendData($Topic);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function CreateGroup(string $GroupName)
+    {
+        $Topic = '/bridge/request/group/add';
+        $Payload = ['friendly_name' => $GroupName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function DeleteGroup(string $GroupName)
+    {
+        $Topic = '/bridge/request/group/remove';
+        $Payload = ['id' => $GroupName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function RenameGroup(string $OldName, string $NewName)
+    {
+        $Topic = '/bridge/request/group/rename';
+        $Payload = ['from' => $OldName, 'to' => $NewName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function AddDeviceToGroup(string $GroupName, string $DeviceName)
+    {
+        $Topic = '/bridge/request/group/members/add';
+        $Payload = ['group'=>$GroupName, 'device' => $DeviceName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function RemoveDeviceFromGroup(string $GroupName, string $DeviceName)
+    {
+        $Topic = '/bridge/request/group/members/remove';
+        $Payload = ['group'=>$GroupName, 'device' => $DeviceName, 'skip_disable_reporting'=>true];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function RemoveAllDevicesFromGroup(string $GroupName)
+    {
+        $Topic = '/bridge/request/group/members/remove_all';
+        $Payload = ['group'=>$GroupName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function Bind(string $SourceDevice, string $TargetDevice)
+    {
+        $Topic = '/bridge/request/device/bind';
+        $Payload = ['from' => $SourceDevice, 'to' => $TargetDevice];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function Unbind(string $SourceDevice, string $TargetDevice)
+    {
+        $Topic = '/bridge/request/device/unbind';
+        $Payload = ['from' => $SourceDevice, 'to' => $TargetDevice];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function RequestNetworkmap()
+    {
+        $Topic = '/bridge/request/networkmap';
+        $Payload = ['type' => 'graphviz', 'routes' => true];
+        return $this->SendData($Topic, $Payload, 0);
+    }
+
+    public function RenameDevice(string $OldDeviceName, string $NewDeviceName)
+    {
+        $Topic = '/bridge/request/device/rename';
+        $Payload = ['from' => $OldDeviceName, 'to' => $NewDeviceName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function RemoveDevice(string $DeviceName)
+    {
+        $Topic = '/bridge/request/device/remove';
+        $Payload = ['id'=>$DeviceName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function CheckOTAUpdate(string $DeviceName)
+    {
+        $Topic = '/bridge/request/device/ota_update/check';
+        $Payload = ['id'=>$DeviceName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+
+    public function PerformOTAUpdate(string $DeviceName)
+    {
+        $Topic = '/bridge/request/device/ota_update/update';
+        $Payload = ['id'=>$DeviceName];
+        $Result = $this->SendData($Topic, $Payload);
+        if ($Result) { //todo check the Response
+            return true;
+        }
+        return false;
+    }
+    private function SendData(string $Topic, array $Payload = [], int $Timeout = 5000)
+    {
+        if ($Timeout) {
+            $TransactionId = $this->AddTransaction($Payload);
+        }
+        $this->SendDebug(__FUNCTION__ . ':Topic', $Topic, 0);
+        $this->SendDebug(__FUNCTION__ . ':Payload', json_encode($Payload), 0);
+        $DataJSON = self::BuildRequest($this->ReadPropertyString('MQTTBaseTopic') . $Topic, $Payload);
         $this->SendDataToParent($DataJSON);
+        if ($Timeout) {
+            $Result = $this->WaitForTransactionEnd($TransactionId, $Timeout);
+            if ($Result === false) {
+                trigger_error($this->Translate('Zigbee2MQTT did not response.'), E_USER_NOTICE);
+                return false;
+            }
+            return $Result;
+        }
+        return true;
+    }
+
+    private function WaitForTransactionEnd(int $TransactionId, int $Timeout)
+    {
+        $Sleep = intdiv($Timeout, 1000);
+        for ($i = 0; $i < 1000; $i++) {
+            $Buffer = $this->TransactionData;
+            if (!array_key_exists($TransactionId, $Buffer)) {
+                return false;
+            }
+            if (count($Buffer[$TransactionId])) {
+                $this->RemoveTransaction($TransactionId);
+                return $Buffer[$TransactionId];
+            }
+            IPS_Sleep($Sleep);
+        }
+        $this->RemoveTransaction($TransactionId);
+        return false;
+    }
+    //################# SENDQUEUE
+
+    private function AddTransaction(array &$Payload)
+    {
+        if (!$this->lock('TransactionData')) {
+            throw new Exception($this->Translate('TransactionData is locked'), E_USER_NOTICE);
+        }
+        $TransactionId = mt_rand(1, 10000);
+        $Payload['transaction'] = $TransactionId;
+        $TransactionData = $this->TransactionData;
+        $TransactionData[$TransactionId] = [];
+        $this->TransactionData = $TransactionData;
+        $this->unlock('TransactionData');
+        return $TransactionId;
+    }
+
+    private function UpdateTransaction(array $Data)
+    {
+        if (!$this->lock('TransactionData')) {
+            throw new Exception($this->Translate('TransactionData is locked'), E_USER_NOTICE);
+        }
+        $TransactionData = $this->TransactionData;
+        if (array_key_exists($Data['transaction'], $TransactionData)) {
+            $TransactionData[$Data['transaction']] = $Data;
+            $this->TransactionData = $TransactionData;
+            $this->unlock('TransactionData');
+            return;
+        }
+        $this->unlock('TransactionData');
+        return;
+    }
+
+    private function RemoveTransaction(int $TransactionId)
+    {
+        if (!$this->lock('TransactionData')) {
+            throw new Exception($this->Translate('TransactionData is locked'), E_USER_NOTICE);
+        }
+        $TransactionData = $this->TransactionData;
+        unset($TransactionData[$TransactionId]);
+        $this->TransactionData = $TransactionData;
+        $this->unlock('TransactionData');
+    }
+
+    private static function BuildRequest(string $Topic, array $Payload)
+    {
+        return json_encode(
+            array_merge(
+                self::$MQTTDataArray,
+                [
+                    'Topic'  => $Topic,
+                    'Payload'=> json_encode($Payload)
+                ]
+            ),
+            JSON_UNESCAPED_SLASHES
+        );
     }
 }
